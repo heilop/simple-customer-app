2cf208000f02fa6a2dd57ca45f1ec8a8de3ca993
Heissen Lopez <heissen.lopez@gmail.com>
2020-01-03 18:58:27 -0500

    Add CustomerData and CustomersActions components

---
+++ src/components/CustomerData.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CustomerData = ({ name, id, age}) => {
+  return (
+    <div>
+      <div className="customer-data">
+        <h2>Customer info</h2>
+        <div><strong>Name<i>{ name }</i></strong></div>
+        <div><strong>ID<i>{ id }</i></strong></div>
+        <div><strong>Age<i>{ age }</i></strong></div>
+      </div>
+    </div>
+  );
+};
+
+CustomerData.propTypes = {
+
+};
+
+export default CustomerData;
