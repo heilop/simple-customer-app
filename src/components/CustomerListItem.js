be487dffa8f33caec230a5e69f2a2a01ded26570
Heissen Lopez <heissen.lopez@gmail.com>
2020-01-03 19:22:59 -0500

    Add customer list item componnent

---
+++ src/components/CustomerListItem.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const CustomerListItem = ({ name, editAction, delAction, urlPath, id }) => {
+  return (
+    <div>
+      <div className="customer-list-item">
+        <div className="field">
+          <Link to={ `${urlPath}${id}` }>{ name }</Link>
+        </div>
+        <div className="field">
+          <Link to={ `${urlPath}${id}/edit` }>{ editAction }</Link>
+        </div>
+        <div className="field">
+          <Link to={ `${urlPath}${id}/del` }>{ delAction }</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+CustomerListItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  editAction: PropTypes.string.isRequired,
+  delAction: PropTypes.string.isRequired,
+  urlPath: PropTypes.string.isRequired,
+};
+
+export default CustomerListItem;
