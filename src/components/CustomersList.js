4bbc7363af2228884d35ac46e268a0d9dfcfd743
Heissen Lopez <heissen.lopez@gmail.com>
2020-01-03 19:32:35 -0500

    Add CustomerList component

---
+++ src/components/CustomersList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import CustomerListItem from './CustomerListItem';
+
+const CustomersList = ({ customers, urlPath }) => {
+  return (
+    <div>
+      <div className="customers-list">
+        {
+          customers.map( c =>
+            <CustomerListItem
+              key = { c.id }
+              name = { c.name }
+              editAction = { 'Edit' }
+              delAction = { 'Delete' }
+              urlPath = { urlPath }
+            >
+            </CustomerListItem>
+
+          )
+        }
+      </div>
+    </div>
+  );
+};
+
+CustomersList.propTypes = {
+  customers: PropTypes.array.isRequired,
+  urlPath: PropTypes.string.isRequired,
+};
+
+export default CustomersList;
