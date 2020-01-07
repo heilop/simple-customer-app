43ea187f88d0b3eb075404457d69d0a652f082c4
Heissen Lopez <heissen.lopez@gmail.com>
2020-01-07 13:55:31 -0500

    Add render body function to CustomerContainer.

--- src/containers/CustomerContainer.js
+++ src/containers/CustomerContainer.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { Route } from 'react-router-dom';
 import AppFrame from './../components/AppFrame';
+import CustomerEdit from './../components/CustomerEdit';
+import CustomerData from './../components/CustomerData';
 import { getCustomerById } from '../selectors/customers';
 
 class CustomerContainer extends Component {
+
+  renderBody = () => (
+    <Route path = "/customers/:id/edit"  children = {
+      ( { match } ) => {
+        const CustomerControl = match ? CustomerEdit : CustomerData;
+        return  <CustomerControl { ...this.props.customer } />
+      }
+    } />
+  );
+
   render() {
     return (
       <div>
         <AppFrame
           header = { `Customer ${this.props.id}` }
-          body = { <p>Customer Data "{ this.props.customer.name }"</p> }
-
+          body = { this.renderBody() }
         >
-
         </AppFrame>
       </div>
     );
