3a944d0acad708aaf7638ecfdf17ab4d9b8c1bc1
Heissen Lopez <heissen.lopez@gmail.com>
2020-01-07 18:33:14 -0500

    Create Higher Order component to set initial Props.

--- src/components/CustomerEdit.js
+++ src/components/CustomerEdit.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { reduxForm, Field } from 'redux-form';
-import { connect } from 'react-redux';
+import { setPropsAsInitial } from './../helpers/setPropsAsInitial';
 
 const CustomerEdit = ({ name, id, age}) => {
   return (
@@ -33,5 +33,4 @@ CustomerEdit.propTypes = {
 };
 
 const CustomerEditForm = reduxForm({ form: 'CustomerEdit'})(CustomerEdit);
-export default connect(
-  (state, props) => ( { initialValues: props } ))(CustomerEditForm);
+export default setPropsAsInitial(CustomerEditForm);
