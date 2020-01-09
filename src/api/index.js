f52a93cd10958cd716f8d530f79bd3005438bb5a
Heissen Lopez <heissen.lopez@gmail.com>
2020-01-09 12:47:43 -0500

    Add update customer feature using fecth PUT.

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,7 @@
 export const apiGet = (url) => () => fetch(url).then(data => data.json());
+
+export const apiPut = (url, id, obj) => () => fetch(`${url}/${id}`, {
+  method: 'PUT',
+  body: JSON.stringify(obj),
+  headers: new Headers({ 'Content-type': 'application/json' })
+}).then(data => data.json())
